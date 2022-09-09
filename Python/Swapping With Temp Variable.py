506c26089068a989c82719d9443bc238ddf36ba1
Prasanth-1011 <112630396+Prasanth-1011@users.noreply.github.com>
2022-09-09 10:57:10 +0530

    Add files via upload

---
+++ Python/Swapping With Temp Variable.py
@@ -0,0 +1,9 @@
+x = str(input("Enter 1st String : "))
+y = str(input("Enter 2nd String : "))
+
+z = x
+x = y
+y = z
+
+print("The Value of Variable 'x' After Swapping :" , x)
+print("The Value of Variable 'y' After Swapping :" , y)
---
+++ Python/Swapping Without Temp Variable.py
@@ -0,0 +1,7 @@
+x = str(input("Enter 1st String : "))
+y = str(input("Enter 2nd String : "))
+
+x , y = y , x
+
+print("The Value of 'x' After Swapping :" , x)
+print("The Value of 'y' After Swapping :" , y)
