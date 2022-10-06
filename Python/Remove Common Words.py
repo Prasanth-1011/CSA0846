d9cf21f92edec6d066b3e45ce4c4075b8f51952f
Prasanth-1011 <112630396+Prasanth-1011@users.noreply.github.com>
2022-10-06 09:26:02 +0530

    Update Remove Common Words.py

--- Python/Remove Common Words.py
+++ Python/Remove Common Words.py
@@ -1,5 +1,4 @@
 from collections import Counter
-
 def removeCommonWords(Sent_1, Sent_2):
 	
 	Sen_1 = list(Sent_1.split())
@@ -8,34 +7,26 @@ def removeCommonWords(Sent_1, Sent_2):
 	Fre_1 = Counter(Sen_1)
 	Fre_2 = Counter(Sen_2)
 
-
-	word = 0
+	Word = 0
 	
 	for i in range(len(Sen_1)):
 		
-		if Sen_1[word] in Fre_2.keys():
+		if Sen_1[Word] in Fre_2.keys():
 			
-			Sen_1.pop(word)
-			
-			word = word-1
-		word += 1
-		
-	word = 0
+			Sen_1.pop(Word)			
+			Word = Word - 1
+		Word += 1
+	Word = 0
 	
 	for i in range(len(Sen_2)):
-		
-		if Sen_2[word] in Fre_1.keys():
+		if Sen_2[Word] in Fre_1.keys():
 			
-			Sen_2.pop(word)
-			
-			word = word-1
-			
-		word += 1
+			Sen_2.pop(Word)
+			Word = Word - 1
+		Word += 1
 	
 	print(*Sen_1)
 	print(*Sen_2)
-
-
 
 Sen_1 = str(input("Enter the Sentence 1 :"))
 Sen_2 = str(input("Enter the Sentence 2 :"))
